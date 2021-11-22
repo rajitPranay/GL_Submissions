225fd8264564e7b8a06bc8864dff47433703908a
rajitPranay <93971395+rajitPranay@users.noreply.github.com>
2021-11-22 17:48:56 +0530

    Update HrDepartment.java

--- GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/implementation/HrDepartment.java
+++ GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/implementation/HrDepartment.java
@@ -2,24 +2,21 @@ package com.gl.ga1.implementation;
 
 import com.gl.ga1.supercls.SuperDepartment;
 
-
 public class HrDepartment extends SuperDepartment {
-	public String departmentName()
-	{
+	public String departmentName() {
 		return "HR Department";
 	}
-	public String getTodaysWork()
-	{
-		return "Fill today’s timesheet and mark your attendance";
+
+	public String getTodaysWork() {
+		return "Fill todayâ€™s timesheet and mark your attendance";
 	}
-	public String getWorkDeadline()
-	{
+
+	public String getWorkDeadline() {
 		return "Complete by EOD ";
 	}
-	public String doActivity()
-	{
+
+	public String doActivity() {
 		return "team Lunch";
 	}
-	
 
 }
