98dc2f96a636abf137ae5d245a2d2ca6be626d87
rajitPranay <93971395+rajitPranay@users.noreply.github.com>
2021-11-22 17:50:24 +0530

    Update SuperDepartment.java

--- GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/supercls/SuperDepartment.java
+++ GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/supercls/SuperDepartment.java
@@ -1,29 +1,20 @@
 package com.gl.ga1.supercls;
 
 public class SuperDepartment {
-//	public String departmantname;
-//	public String getTodaysWork;
-//	public String getWorkDeadline;
-//	public String isTodayAHoliday;
-//	public String doActivity;
-//	public String getTechStackInformation;
 
-	
-	
-	public String departmentName()
-	{
+	public String departmentName() {
 		return "Super Department";
 	}
-	public String getTodaysWork()
-	{
+
+	public String getTodaysWork() {
 		return "No Work as of now";
 	}
-	public String getWorkDeadline()
-	{
+
+	public String getWorkDeadline() {
 		return "Nil";
 	}
-	public String isTodayAHoliday()
-	{
+
+	public String isTodayAHoliday() {
 		return "Today is not a holiday";
 	}
 
