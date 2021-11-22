fa804883fd11ab83f35fc92fc23888b971eb0cbb
rajitPranay <93971395+rajitPranay@users.noreply.github.com>
2021-11-22 17:47:53 +0530

    Update AdminDepartment.java

--- GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/implementation/AdminDepartment.java
+++ GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/implementation/AdminDepartment.java
@@ -2,19 +2,17 @@ package com.gl.ga1.implementation;
 
 import com.gl.ga1.supercls.SuperDepartment;
 
-public class AdminDepartment extends SuperDepartment  {
-	public String departmentName()
-	{
+public class AdminDepartment extends SuperDepartment {
+	public String departmentName() {
 		return "Admin Department";
 	}
-	public String getTodaysWork()
-	{
+
+	public String getTodaysWork() {
 		return "Complete your documents Submission";
 	}
-	public String getWorkDeadline()
-	{
+
+	public String getWorkDeadline() {
 		return "Complete by EOD ";
 	}
-	
 
 }
