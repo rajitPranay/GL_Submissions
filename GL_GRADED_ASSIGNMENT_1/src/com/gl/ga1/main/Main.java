0bb022045b21d81aa18fdf111cfc5f048f7a3762
rajitPranay <93971395+rajitPranay@users.noreply.github.com>
2021-11-22 17:49:54 +0530

    Update Main.java

--- GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/main/Main.java
+++ GL_GRADED_ASSIGNMENT_1/src/com/gl/ga1/main/Main.java
@@ -5,8 +5,6 @@ import com.gl.ga1.implementation.HrDepartment;
 import com.gl.ga1.implementation.TechDepartment;
 import com.gl.ga1.supercls.SuperDepartment;
 
-
-
 public class Main {
 	public static void main(String[] args) {
 		SuperDepartment admin = new AdminDepartment(); // created the object using upcating
@@ -14,28 +12,26 @@ public class Main {
 		System.out.println(admin.getTodaysWork());
 		System.out.println(admin.getWorkDeadline());
 		System.out.println(admin.isTodayAHoliday());
-		
+
 		System.out.println("");
-		
+
 		HrDepartment hr = new HrDepartment();
-		
-		System.out.println(" Welcome to " +hr.departmentName());
+
+		System.out.println(" Welcome to " + hr.departmentName());
 		System.out.println(hr.doActivity());
 		System.out.println(hr.getTodaysWork());
 		System.out.println(hr.getWorkDeadline());
 		System.out.println(hr.isTodayAHoliday());
 
-		
 		System.out.println("");
 
 		TechDepartment tech = new TechDepartment();
-		System.out.println(" Welcome to " +tech.departmentName());
+		System.out.println(" Welcome to " + tech.departmentName());
 		System.out.println(tech.getTodaysWork());
 		System.out.println(tech.getWorkDeadline());
 		System.out.println(tech.getTechStackInformation());
 		System.out.println(tech.isTodayAHoliday());
 
-		
 	}
 
 }
